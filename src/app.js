e6a133cb9d38fbd9169a8228ca96048417945e11
ramesh-b-marmeto <ramesh.b@marmeto.com>
2025-01-25 18:07:29 +0530

    exploring express and routes with postman .

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ const express = require('express');
 const app = express();
 
 app.use("/test",(req,res)=>{
-  res.send("Hello from server : AS")
+  res.send("Hello from server : AS");
+})
+
+app.get("/user", (req,res)=>{
+  res.send({fname: "Ramesh" , lname:"B"});
+})
+
+app.post("/user/:groupid", (req,res)=>{
+  console.log(req.query)
+  console.log(req.params)
+  res.send("Data stored");
 })
 
 app.listen(9709,()=>{
-  console.log('Server is listening')
+  console.log('Server is listening');
 });
