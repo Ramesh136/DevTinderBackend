7b81987375155d0ff8fbd40fc779f48f145d3a80
Ramesh136 <88448065+Ramesh136@users.noreply.github.com>
2025-08-14 13:05:27 +0530

    removed logs

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/users");
 
 const authUser = async (req , res , next)=>{
-  console.log("req",req.cookies);
   try{
     const {token} = req.cookies ;
     if(!token){
