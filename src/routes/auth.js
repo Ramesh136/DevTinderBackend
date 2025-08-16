d14390631c5fa6969daad5844ee3426b8e99311c
Ramesh136 <88448065+Ramesh136@users.noreply.github.com>
2025-08-16 11:15:19 +0530

    updated cookie policy

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -31,7 +31,6 @@ authRouter.post("/signup", async (req, res) => {
     if (token) {
       res.cookie("token", token, { 
         expires: new Date(Date.now() + 8 * 3600000),
-        httpOnly: true,
         secure: true, // use false ONLY for local dev without HTTPS
         sameSite: 'none'
        });
@@ -70,7 +69,6 @@ authRouter.post("/login", async (req, res) => {
       if (token) {
         res.cookie("token", token, { 
           expires: new Date(Date.now() + 8 * 3600000),
-          httpOnly: true,
           secure: true, // use false ONLY for local dev without HTTPS
           sameSite: 'none'
         });
@@ -100,7 +98,6 @@ authRouter.post("/login", async (req, res) => {
 authRouter.post("/logout", (req, res) => {
   res.cookie("token", null, {
     expires: new Date(Date.now()),
-    httpOnly: true,
     secure: true, // use false ONLY for local dev without HTTPS
     sameSite: 'none'
   });
